85b026376bb1106572866ed8c72cc7136b33eb7c
ArihantSrivastava2225/ <arihantsrivastava71011@gmail.com>
2025-08-14 12:37:39 +0530

    training on more dataset

--- train/train.py
+++ train/train.py
@@ -115,7 +115,8 @@ def train(dataset_dir, year, split, epochs=50, batch_size=8, save_dir='checkpoin
     dataset = RefCOCODataset(ann_file, img_root, image_size=image_size, tokenizer=tokenizer)
     
     def gen():
-        for i in range(len(dataset)):
+        max_samples = 10000
+        for i in range(min(len(dataset), max_samples)):
             yield dataset[i]
 
     out_types = (tf.float32, tf.int32, tf.int32, tf.float32)
